1ba48088c6f22445f2f392c5906de051d45bee91
tavish <tavish@paywithextend.com>
2022-10-16 22:24:47 -0500

    Some minor changes while setting up dev env and adding a gitignore

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,9 @@ module Main (main) where
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 
-
 -- Modeling the Game
 newtype Card = Card (Set.Set Text.Text)
+
 newtype Game = Game (Set.Set Card)
 
 -- Implement necessary Typeclasses for our models so
@@ -45,30 +45,29 @@ generateSportItCards :: Set.Set Text.Text -> Game
 generateSportItCards _ = exampleGame
 
 generateCardsWithSize :: Int -> Set.Set Text.Text -> Set.Set Card
-generateCardsWithSize size symbols = let
-  go :: State -> Set.Set Card -> Set.Set Card
-  go state@(State size _ _) cards = if (cardIsSize card' size)
-      then go newState (Set.insert card' cards)
-    else cards
-    where (newState, card') = makeCard state
+generateCardsWithSize size symbols =
+  let go :: State -> Set.Set Card -> Set.Set Card
+      go state@(State size _ _) cards =
+        if cardIsSize card' size
+          then go newState (Set.insert card' cards)
+          else cards
+        where
+          (newState, card') = makeCard state
 
-  initState = State size (Set.drop 1 symbols) (Set.take 1 symbols)
-  in go initState Set.empty
-
-
-
-
+      initState = State size (Set.drop 1 symbols) (Set.take 1 symbols)
+   in go initState Set.empty
 
 data State = State Int (Set.Set Text.Text) (Set.Set Text.Text)
-makeCard :: State -> (State, Card)
-makeCard (State size unusedSymbols onceUsedSymbols) = let
-  unmatched = Set.take (size - 1) unusedSymbols
-  match = Set.take 1 onceUsedSymbols
-  newState = State size (Set.drop (size - 1) unusedSymbols) (Set.union (Set.drop 1 onceUsedSymbols) unmatched)
-  card = Card (Set.union match unmatched)
-  in (newState, card)
 
-initialState :: Int -> (Set.Set Text.Text) -> State
+makeCard :: State -> (State, Card)
+makeCard (State size unusedSymbols onceUsedSymbols) =
+  let unmatched = Set.take (size - 1) unusedSymbols
+      match = Set.take 1 onceUsedSymbols
+      newState = State size (Set.drop (size - 1) unusedSymbols) (Set.union (Set.drop 1 onceUsedSymbols) unmatched)
+      card = Card (Set.union match unmatched)
+   in (newState, card)
+
+initialState :: Int -> Set.Set Text.Text -> State
 initialState size symbols = State size symbols Set.empty
 
 -- Some analysis functions
@@ -83,7 +82,8 @@ getMatch :: Card -> Card -> Maybe Text.Text
 getMatch a b
   | Set.size matches == 1 = Just $ Set.elemAt 0 matches
   | otherwise = Nothing
-  where matches = getMatches a b
+  where
+    matches = getMatches a b
 
 getMatches :: Card -> Card -> Set.Set Text.Text
 getMatches (Card as) (Card bs) =
@@ -95,30 +95,77 @@ isOne _ = False
 
 -- Example data to use while testing / developing
 hand = "🤟"
+
 eye = "👁"
+
 brain = "🧠"
+
 tooth = "🦷"
+
 bone = "🦴"
+
 muscle = "💪"
+
 baby = "👶"
+
 ball = "⚽️"
+
 violin = "🎻"
+
 watch = "⌚️"
+
 phone = "📱"
+
 laptop = "💻"
+
 keyboard = "⌨️"
+
 printer = "🖨"
+
 joystick = "🕹"
+
 cd = "💽"
+
 floppydisk = "💾"
+
 vhs = "📼"
+
 camera = "📷"
 
-symbols = toSet ["hand", "eye", "brain", "tooth", "bone", "muscle", "baby", "ball", "violin", "watch", "phone", "laptop", "keyboard", "printer", "joystick", "cd", "floppydisk", "vhs", "camera"]
+symbols :: Set.Set Text.Text
+symbols =
+  toSet
+    [ "hand",
+      "eye",
+      "brain",
+      "tooth",
+      "bone",
+      "muscle",
+      "baby",
+      "ball",
+      "violin",
+      "watch",
+      "phone",
+      "laptop",
+      "keyboard",
+      "printer",
+      "joystick",
+      "cd",
+      "floppydisk",
+      "vhs",
+      "camera"
+    ]
+
+letters :: Set.Set Text.Text
 letters = toSet ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+exampleCard1 :: Card
 exampleCard1 = Card (toSet ["bird", "cat", "eyeball"])
+
+exampleCard2 :: Card
 exampleCard2 = Card (toSet ["dog", "bird", "cactus"])
+
+exampleGame :: Game
 exampleGame = Game (Set.fromList [exampleCard1, exampleCard2])
 
 main :: IO ()
